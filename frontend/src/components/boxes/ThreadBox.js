7ef9d0364775ecb687e89843320f1f2337fe017f
UisDangerouz <matias.rantala@hotmail.com>
2020-04-15 16:37:05 +0300

    Added time difference calculations to threads

--- frontend/src/components/boxes/ThreadBox.js
+++ frontend/src/components/boxes/ThreadBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import socket from '../../services/connect'
 
-const ThreadBox = ({id, text, likes, location, color}) => {
+const ThreadBox = ({id, text, likes, location, color, time}) => {
 
   const colorIndex = color ? color : 0
 
@@ -26,20 +26,34 @@ const ThreadBox = ({id, text, likes, location, color}) => {
     marginBottom: '1%'
   }
   
-    return (
-      <div>
-        <button onClick={e => handleThreadClick(e, id)}  style={styles} className='message'>  
-          <div className="multilineText">{text}</div>
-          <br></br>
-          <span role="img" aria-label='Location'>📍</span>{location}
-        </button>
-        <div className="messageLikeContainer">
-          <button className="likeButton">▲</button>
-          <p id="likeText">{likes}</p>
-          <button className="likeButton">▼</button>
-        </div>
+  let timeDifference = (new Date() - time) / 1000;
+  let timeText = ''
+  if(timeDifference < 60) {
+    timeText = Math.round(timeDifference) + "s"
+  } else if(timeDifference < 3600) {
+    timeText = Math.round(timeDifference / 60) + "min"
+  } else if(timeDifference < 86400) {
+    timeText = Math.round(timeDifference / 3600) + "h"
+  } else {
+    timeText = Math.round(timeDifference / 86400) + "d"
+  }
+
+  return (
+    <div>
+      <button onClick={e => handleThreadClick(e, id)}  style={styles} className='message'>  
+        <div className="multilineText">{text}</div>
+        <br></br>
+  <span role="img" aria-label='Location'>{timeText} ago 📍{location}</span>
+      </button>
+      <div className="messageLikeContainer">
+        <button className="likeButton">▲</button>
+        <p id="likeText">{likes}</p>
+        <button className="likeButton">▼</button>
       </div>
-    )
+    </div>
+  )
+
+
   }
   
   const handleThreadClick = (event, id) => {

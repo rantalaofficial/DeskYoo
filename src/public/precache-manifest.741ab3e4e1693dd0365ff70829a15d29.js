cb2f6a2179734107481ce2c28185bede548c85d7
Aapo Laakkio <al3329@edu.turku.fi>
2020-04-19 22:33:52 +0300

    New frontend build

---
+++ src/public/precache-manifest.741ab3e4e1693dd0365ff70829a15d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcc2ce0632f873cc1366379a6df236e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bbad8c54cb239b36728",
+    "url": "/static/css/main.5c825b36.chunk.css"
+  },
+  {
+    "revision": "37e17fe69083df5d7d9d",
+    "url": "/static/js/2.19c7a402.chunk.js"
+  },
+  {
+    "revision": "aa7dc884f632096396692d395e836b8c",
+    "url": "/static/js/2.19c7a402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbad8c54cb239b36728",
+    "url": "/static/js/main.60482227.chunk.js"
+  },
+  {
+    "revision": "02646f041d941998c57b",
+    "url": "/static/js/runtime-main.60a7823c.js"
+  }
+]);

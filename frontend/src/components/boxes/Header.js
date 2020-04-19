15d16bf330736e8ad5f0540d9cae70a57ca5294b
Aapo Laakkio <al3329@edu.turku.fi>
2020-04-19 23:06:19 +0300

    Move logo

--- frontend/src/components/boxes/Header.js
+++ frontend/src/components/boxes/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => (
     <div className="greenBox" id="Header">
-      <img src="https://raw.githubusercontent.com/UisDangerouz/DeskYoo/master/logo.png" alt="DeskYoo" width="400px" height="auto"></img>
+      <img src="logo.png" alt="DeskYoo" width="400px" height="auto"></img>
     </div>
 )
 
